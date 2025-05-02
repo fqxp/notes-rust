a373bc8e196dd5fddef7fe8848d40529f25e8a1e
Frank Ploss <mail@fqxp.de>
2025-05-02 13:53:08 +0200

    show note name instead of filename

--- src/note_list.rs
+++ src/note_list.rs
@@ -15,6 +15,15 @@ pub enum NoteListOutput {
     SelectFile(usize),
 }
 
+fn display_filename(filename: &String) -> &str {
+    let base_filename = filename.rsplit("/").next().unwrap();
+
+    return match base_filename.strip_suffix(".md") {
+        Some(name) => name,
+        None => base_filename,
+    };
+}
+
 #[relm4::factory(pub)]
 impl FactoryComponent for NoteListItem {
     type Init = String;
@@ -36,7 +45,7 @@ impl FactoryComponent for NoteListItem {
                 set_margin_top: 4,
                 set_margin_bottom: 4,
                 #[watch]
-                set_label: &self.filename,
+                set_label: display_filename(&self.filename),
             },
         }
     }
